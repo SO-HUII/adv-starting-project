6de93c3590b2e422311b152d24c562732b507f9f
SO HUI <haejuuun925@gmail.com>
2024-01-19 16:27:45 +0900

    feat: 5개 페이지 추가 및 라우트 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/Home';
+import EventPage from './pages/Event';
+import EventDetailPage from './pages/EventDetail';
+import NewEventPage from './pages/NewEvent';
+import EditEventPage from './pages/EditEvent';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage/> },
+  { path: '/events', element: <EventPage /> },
+  { path: '/events/:eventId', element: <EventDetailPage /> },
+  { path: '/events/new', element: <NewEventPage /> },
+  { path: '/events/:eventId/edit', element: <EditEventPage /> }
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
