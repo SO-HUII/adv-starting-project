08e85da49e4ebe993ef46adf82ba6f12c8fcf43a
SO HUI <haejuuun925@gmail.com>
2024-01-20 03:47:04 +0900

    feat; root layot 생성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,18 @@ import EventPage from './pages/Event';
 import EventDetailPage from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
+import RootLayout from './pages/Root';
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage/> },
-  { path: '/events', element: <EventPage /> },
-  { path: '/events/:eventId', element: <EventDetailPage /> },
-  { path: '/events/new', element: <NewEventPage /> },
-  { path: '/events/:eventId/edit', element: <EditEventPage /> }
+  {path: '/', element: <RootLayout />, 
+    children: [
+      { index: true, element: <HomePage/> },
+      { path: 'events', element: <EventPage /> },
+      { path: 'events/:eventId', element: <EventDetailPage /> },
+      { path: 'events/new', element: <NewEventPage /> },
+      { path: 'events/:eventId/edit', element: <EditEventPage /> }
+    ]
+  },
 ]);
 
 function App() {
